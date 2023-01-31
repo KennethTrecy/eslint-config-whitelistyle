46152e0ffe5563de01ecb48534e7c3b9ca302490
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2023-01-31 21:58:04 +0800

    feat: remove certain settings that maybe different per project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 module.exports = {
-	"parserOptions": {
-		"ecmaVersion": 2020,
-		"sourceType": "module"
-	},
-	"env": {
-		"browser": true,
-		"es6": true,
-		"node": true
-	},
 	"extends": [
 		"eslint:all"
 	],
