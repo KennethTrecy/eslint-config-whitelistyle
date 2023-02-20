a5e907e8f4dfb2ad43b225147bcc18c3e5528f43
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-21 00:15:17 +0800

    feat: add personal configurations for projects using Vue.js framework

---
+++ src/vue.js
@@ -0,0 +1,138 @@
+module.exports = {
+	"rules": {
+		"vue/html-indent": [
+			"error",
+			"tab",
+			{
+				"attribute": 1,
+				"baseIndent": 1,
+				"closeBracket": 0,
+				"alignAttributesVertically": true,
+				"ignores": []
+			}
+		],
+		"vue/max-attributes-per-line": [
+			"error",
+			{
+				"singleline": {
+					"max": 2,
+					"allowFirstLine": true
+				},
+				"multiline": {
+					"max": 1,
+					"allowFirstLine": false
+				}
+			}
+		],
+		"vue/html-closing-bracket-newline": [
+			"error",
+			{
+				"singleline": "never",
+				"multiline": "never"
+			}
+		],
+		"vue/html-closing-bracket-spacing": [
+			"error",
+			{
+				"startTag": "never",
+				"endTag": "never",
+				"selfClosingTag": "never"
+			}
+		],
+		"vue/component-tags-order": [
+			"error",
+			{
+				"order": [
+					"template",
+					"style",
+					"script"
+				]
+			}
+		],
+		"vue/sort-keys": [
+			"warn",
+			"asc",
+			{
+				"caseSensitive": true,
+				"ignoreChildrenOf": [
+					"methods"
+				],
+				"ignoreGrandchildrenOf": [
+					"directives",
+					"inject",
+					"watch"
+				],
+				"minKeys": 2,
+				"natural": false
+			}
+		],
+		"vue/html-self-closing": [
+			"error",
+			{
+				"html": {
+					"void": "always",
+					"normal": "never",
+					"component": "always"
+				},
+				"svg": "always",
+				"math": "always"
+			}
+		],
+		"vue/order-in-components": [
+			"error",
+			{
+				"order": [
+					"el",
+					"name",
+					"key",
+					"parent",
+					"functional",
+					[
+						"delimiters",
+						"comments"
+					],
+					[
+						"directives",
+						"filters"
+					],
+					"extends",
+					"mixins",
+					[
+						"provide",
+						"inject"
+					],
+					"ROUTER_GUARDS",
+					"layout",
+					"middleware",
+					"validate",
+					"scrollToTop",
+					"transition",
+					"loading",
+					"inheritAttrs",
+					"model",
+					[
+						"props",
+						"propsData"
+					],
+					"emits",
+					"components",
+					"setup",
+					"asyncData",
+					"data",
+					"fetch",
+					"head",
+					"computed",
+					"watch",
+					"watchQuery",
+					"LIFECYCLE_HOOKS",
+					"methods",
+					[
+						"template",
+						"render"
+					],
+					"renderError"
+				]
+			}
+		]
+	}
+}
